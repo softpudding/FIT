baa7bde71d9c8fea43a9702ea04c63650b7e36de
yangxuanhui <578986944@qq.com>
2019-07-14 23:51:43 +0800

    修改界面

--- code/FitMvp/app/src/main/java/com/example/fitmvp/view/fragment/FragmentMe.java
+++ code/FitMvp/app/src/main/java/com/example/fitmvp/view/fragment/FragmentMe.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
@@ -18,14 +19,12 @@ public class FragmentMe extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.me, container, false);
-
-
         return view;
     }
     @Override
     public void onActivityCreated(Bundle bundle){
         super.onActivityCreated(bundle);
-        ImageButton toSetting = getActivity().findViewById(R.id.setting_button);
+        Button toSetting = getActivity().findViewById(R.id.button_setting);
         toSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
