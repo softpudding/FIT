c24d0c25f55324fde73d12b659e1578c9f61195c
IcyFrog <sjtu-yqc@sjtu.edu.cn>
2019-07-17 14:32:44 +0800

    delete mvp

--- code/FitMvp/app/src/main/java/com/example/fitmvp/MyService.java
+++
@@ -1,6 +0,0 @@
-package com.example.fitmvp;
-
-import cn.jpush.android.service.JCommonService;
-
-public class MyService extends JCommonService {
-}
--- code/FitMvp/app/src/main/java/com/example/fitmvp/model/MeModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.fitmvp.model;
-
-import com.example.fitmvp.contract.MeContract;
-
-public class MeModel implements MeContract.Model {
-}
